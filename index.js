a3bf3a800d4e47f6e778001af64e55c98fae7588
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2023-08-12 21:14:01 +0100

    Refactor moveRight and moveLeft functionality

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-const img = document.getElementsByClassName('carousel')[0];
+const firstGalleryCarouselImg = document.getElementsByClassName('carousel')[0];
 const rightBtn = document.getElementById('right-btn');
 const leftBtn = document.getElementById('left-btn');
+
+let position = 0;
 
 // Take images from HTML
 let pictures = [];
 let carouselItems = document.getElementsByClassName("carousel");
+
 for (const element of carouselItems) {
     pictures.push(element.src);
 };
 
-let position = 0;
-
 const moveRight = () => {
-    if (position >= pictures.length - 1) {
-        position = 0;
-        img.src = pictures[position];
-        return;
-    }
-    img.src = pictures[++position];
+    position = sanitizeCarouselIndex(position + 1, 3);
+    changeGalleryCarouselImg(position);
 }
 
 const moveLeft = () => {
-    if (position < 1) {
-        position = pictures.length - 1;
-        img.src = pictures[position];
-        return;
-    }
-    img.src = pictures[--position];
+    position = sanitizeCarouselIndex(position - 1, 3);
+    changeGalleryCarouselImg(position);
+}
+
+function changeGalleryCarouselImg(idx) {
+    firstGalleryCarouselImg.src = pictures[idx];
+}
+
+function sanitizeCarouselIndex(newPosition, carouselSize) {
+    return newPosition % carouselSize;
 }
 
 const mql = window.matchMedia("(max-width: 1052px)");
 
-mql.onchange = (e) => {
-    img.src = pictures[0];
+mql.onchange = () => {
+    changeGalleryCarouselImg(0);
 };
 
 rightBtn.addEventListener("click", moveRight);
