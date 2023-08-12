8aabc39ec7defff5dff5b10b789860c717c7963f
Yuneidy Castillo <yuneidycastillo25@gmail.com>
2023-08-12 16:22:49 +0100

    Move image URL to HTML

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const img = document.getElementsByClassName('carousel')[0];
 const rightBtn = document.getElementById('right-btn');
 const leftBtn = document.getElementById('left-btn');
 
-// Images are from unsplash
-let pictures = ['./assets/img/gallery-1.jpg', './assets/img/gallery-2.jpg', './assets/img/gallery-3.jpg'];
+// Take images from HTML
+let pictures = [];
+let carouselItems = document.getElementsByClassName("carousel");
+for (const element of carouselItems) {
+    pictures.push(element.src);
+};
 
-img.src = pictures[0];
 let position = 0;
 
 const moveRight = () => {
     if (position >= pictures.length - 1) {
-        position = 0
+        position = 0;
         img.src = pictures[position];
         return;
     }
-    img.src = pictures[position + 1];
-    position++;
+    img.src = pictures[++position];
 }
 
 const moveLeft = () => {
@@ -24,20 +26,14 @@ const moveLeft = () => {
         img.src = pictures[position];
         return;
     }
-    img.src = pictures[position - 1];
-    position--;
+    img.src = pictures[--position];
 }
 
 const mql = window.matchMedia("(max-width: 1052px)");
 
 mql.onchange = (e) => {
     img.src = pictures[0];
-  };
+};
 
 rightBtn.addEventListener("click", moveRight);
 leftBtn.addEventListener("click", moveLeft);
-
-let carousel_imgs = document.getElementsByClassName("carousel");
-carousel_imgs[0].src = pictures[0];
-carousel_imgs[1].src = pictures[1];
-carousel_imgs[2].src = pictures[2];
